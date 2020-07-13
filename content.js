733ff4e6b0f884877877e15b9c42807a4629c257
Oscar Litorell <oscar.litorell@gmail.com>
2020-07-13 18:43:07 +0200

    Timestamps are now in seconds.

--- content.js
+++ content.js
@@ -2,12 +2,8 @@ function setTimestamp() {
     let t = document.querySelector(".ytp-time-current").innerText.split(":")
     let addr = location.href.split("?")
     let params = new URLSearchParams(addr[1])
-    
-    if (params.length == 1) {
-        params.set("t", `${t[0]}m${t[1]}s`)
-    } else {
-        params.set("t", `${t[0]}h${t[1]}m${t[2]}s`)
-    }
+    let seconds = t.reduce((prev, curr) => prev * 60 + Number(curr), 0)
+    params.set("t", `${seconds}s`)
     location.href = `${addr[0]}?${params.toString()}`
 }
 
