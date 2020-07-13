e3e69f7d464ae956c87fca9e6524fbcdfa9de6de
Oscar Litorell <oscar.litorell@gmail.com>
2020-07-13 18:38:40 +0200

    Added support for hour long videos

--- content.js
+++ content.js
@@ -2,7 +2,12 @@ function setTimestamp() {
     let t = document.querySelector(".ytp-time-current").innerText.split(":")
     let addr = location.href.split("?")
     let params = new URLSearchParams(addr[1])
-    params.set("t", `${t[0]}m${t[1]}s`)
+    
+    if (params.length == 1) {
+        params.set("t", `${t[0]}m${t[1]}s`)
+    } else {
+        params.set("t", `${t[0]}h${t[1]}m${t[2]}s`)
+    }
     location.href = `${addr[0]}?${params.toString()}`
 }
 
